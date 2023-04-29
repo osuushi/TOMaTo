5f9753996644a9f8a333c35b778a0da60ee42d93
Ada Cohen <ada@fieldbook.com>
2023-04-29 12:09:26 -0400

    Fix types for builtin model

--- src/renderer/builtins.ts
+++ src/renderer/builtins.ts
@@ -1,3 +1,4 @@
+import { ModelName } from "../shared/storage";
 import { Chitchat } from "../shared/storage";
 
 type ChitchatWithoutUuid = Omit<Chitchat, "uuid">;
@@ -6,7 +7,7 @@ const builtinsWithoutUuids: ChitchatWithoutUuid[] = [{
   mnemonic: "th",
   fullName: "Thesaurus",
   description: "A loose thesaurus that finds similar words and phrases",
-  model: "gpt-3.5-turbo",
+  model: ModelName.Gpt35Turbo,
   promptChain: [
     "Produce a bulleted list with words or phrases that are similar to %s. Do not include any other text"
   ],
@@ -15,7 +16,7 @@ const builtinsWithoutUuids: ChitchatWithoutUuid[] = [{
   mnemonic: "rd",
   fullName: "Reverse Dictionary",
   description: "Look up a word by its definition and/or connotations",
-  model: "gpt-3.5-turbo",
+  model: ModelName.Gpt35Turbo,
   promptChain: [
     "What are some words for %s? Put them in a bulleted list, and do not include any other text."
   ]
@@ -24,7 +25,7 @@ const builtinsWithoutUuids: ChitchatWithoutUuid[] = [{
   mnemonic: "emod",
   fullName: "Emotions Dictionary",
   description: "Find behaviors associated with a particular emotion",
-  model: "gpt-3.5-turbo",
+  model: ModelName.Gpt35Turbo,
   promptChain: [
     "What are some behaviors a person might display if they were feeling %s? Put them in a bulleted list, and do not include any other text."
   ]
@@ -33,7 +34,7 @@ const builtinsWithoutUuids: ChitchatWithoutUuid[] = [{
   mnemonic: "emoth",
   fullName: "Emotions Thesaurus",
   description: "Find behaviors similar to another behavior",
-  model: "gpt-3.5-turbo",
+  model: ModelName.Gpt35Turbo,
   promptChain: [
     "What are some alternative behaviors that convey the same emotion as %s? Put them in a bulleted list, and do not include any other text."
   ]
@@ -42,7 +43,7 @@ const builtinsWithoutUuids: ChitchatWithoutUuid[] = [{
   mnemonic: "haiku",
   fullName: "Haiku",
   description: "Write a haiku about a topic",
-  model: "gpt-3.5-turbo",
+  model: ModelName.Gpt35Turbo,
   promptChain: [
     "Write a short paragraph expanding on the topic of %s.",
     `Write a haiku based on the thoughts above. Avoid words that are too similar to "%s".`
